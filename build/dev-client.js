39f4e2114d64efef1d308f9a0697201332917034
chenchen <chenchen3@eastmoney.com>
2017-08-17 18:19:36 +0800

    add cors support. set _webpack_require_.p to localhost:6001.

--- build/dev-client.js
+++ build/dev-client.js
@@ -5,6 +5,9 @@ let port= require('../input').devport
 
 var hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true&path=http://localhost:6001/__webpack_hmr')
 
+
+ __webpack_require__.p="http://localhost:6001/"
+
 hotClient.subscribe(function (event) {
   if (event.action === 'reload') {
     window.location.reload()
